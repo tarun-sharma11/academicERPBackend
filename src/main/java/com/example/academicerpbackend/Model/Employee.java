bacc96463a141369042b50044694ac536fa2e0a9
tarun <ts137137@gmail.com>
2023-11-27 18:28:32 +0530

    Entity Completed

---
+++ src/main/java/com/example/academicerpbackend/Model/Employee.java
@@ -0,0 +1,93 @@
+package com.example.academicerpbackend.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "EMPLOYEES")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long employee_id;
+
+    public Employee() {
+    }
+
+    public Employee(String first_name, String last_name, String email, String title, String photograph_path, long department_id) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.title = title;
+        this.photograph_path = photograph_path;
+        this.department_id = department_id;
+    }
+
+    @Column(name = "FIRST_NAME")
+    private String first_name;
+    @Column(name = "LAST_NAME")
+    private String last_name;
+
+    @Column(name = "EMAIL")
+    private String email;
+    @Column(name = "TITLE")
+    private String title;
+    @Column(name = "PHOTOGRAPH_PATH")
+    private String photograph_path;
+    @Column(name = "DEPARTMEFIRST_NAMENT_ID")
+    private long department_id;
+
+    public long getEmployee_id() {
+        return employee_id;
+    }
+
+    public void setEmployee_id(long employee_id) {
+        this.employee_id = employee_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPhotograph_path() {
+        return photograph_path;
+    }
+
+    public void setPhotograph_path(String photograph_path) {
+        this.photograph_path = photograph_path;
+    }
+
+    public long getDepartment_id() {
+        return department_id;
+    }
+
+    public void setDepartment_id(long department_id) {
+        this.department_id = department_id;
+    }
+}
